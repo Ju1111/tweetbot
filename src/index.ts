e961bbddb64af94a59eb04f148727006c732ea87
Ju111 <SchneiderJu@gmx.net>
2018-07-18 12:15:31 +0200

    connect to personal twitter app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const welcome: string = 'Hello World'
-console.log(welcome)
+import * as Twit from 'twit'
+
+const twit = new Twit({
+    consumer_key: '	mWpZUIBfTbQw9woUb0NBFiOC1',
+    consumer_secret: 'FuG0rhBtNhpcO3MKhWljzVp0sbaXSScsFqN6pNAu9rtjDnANXv',
+    access_token: '1019499453932285952-KrKT8EkTQDvqlW4oqX9Mr6yURgfo7r',
+    access_token_secret: '1VY0Hj5qklO0KZEKOJHlrlklawVCrsRaqplUp1yHghwh8'
+})
+
+const fn = async () => {
+    // await something
+  }
+  fn()
